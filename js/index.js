81281283d0d610df0751947e114acf40d88bca7d
git_user_name <rghgbnupisj@gmail.com>
2025-08-31 16:31:41 +0530

    added favicon icon

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 // message on index page
   window.addEventListener('load', () => {
+
     // Show modal only if not shown before
-    // if (!localStorage.getItem('developerModalShown')) {
-    //   document.getElementById('customModal').classList.remove('hidden');
-    //   localStorage.setItem('developerModalShown', 'true');
-    // }
+    if (!localStorage.getItem('developerModalShown')) {
+      document.getElementById('customModal').classList.remove('hidden');
+      localStorage.setItem('developerModalShown', 'true');
+    }
     
     // Always show the modal, every page load
-    document.getElementById('customModal').classList.remove('hidden');
+    // document.getElementById('customModal').classList.remove('hidden');
 
   });
   function closeModal() {
